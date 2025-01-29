836af5ac58a027a3598844e493a9d16e557aff77
Turnergamer <63259043+Turnergamer@users.noreply.github.com>
2025-01-29 12:51:30 +0000

    Update Premium.lua

--- src/Premium.lua
+++ src/Premium.lua
@@ -1,3 +1,7 @@
 local version = tonumber(game:HttpGet("https://raw.githubusercontent.com/Turnergamer/ProX/refs/heads/main/src/Version", true))
 
-print(version)
+if version ~= 1 then
+    game.Players.LocalPlayer:Kick("Your version is not valid. Please update.")
+else
+    print("Version is valid.")
+end

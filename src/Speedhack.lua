5624a0d7962100585d06d4a288966a0de6571826
Turnergamer <63259043+Turnergamer@users.noreply.github.com>
2025-01-29 08:43:19 +0000

    d

--- src/Speedhack.lua
+++ src/Speedhack.lua
@@ -1,1 +1,13 @@
-a
+local tpwalking = true
+
+game:GetService("RunService").Heartbeat:Connect(function(delta)
+	if tpwalking then
+		local chr = game.Players.LocalPlayer.Character
+		local hum = chr and chr:FindFirstChildWhichIsA("Humanoid")
+
+		if hum and hum.MoveDirection.Magnitude > 0 then
+			local tpValue = 5  -- Always use 5 as the teleport distance
+			chr:TranslateBy(hum.MoveDirection * tpValue * delta * 10)  -- Apply teleportation
+		end
+	end
+end)

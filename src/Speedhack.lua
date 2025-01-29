ae728f0b434443f20bddfb8521bb6ad88766057b
Turnergamer <63259043+Turnergamer@users.noreply.github.com>
2025-01-29 09:06:31 +0000

    PRO

--- src/Speedhack.lua
+++ src/Speedhack.lua
@@ -1,13 +1,16 @@
-local tpwalking = true
+local tpwalking = false  -- Start with tpwalking off
 
 game:GetService("RunService").Heartbeat:Connect(function(delta)
-	if tpwalking then
-		local chr = game.Players.LocalPlayer.Character
-		local hum = chr and chr:FindFirstChildWhichIsA("Humanoid")
+    local chr = game.Players.LocalPlayer.Character
+    local hum = chr and chr:FindFirstChildWhichIsA("Humanoid")
 
-		if hum and hum.MoveDirection.Magnitude > 0 then
-			local tpValue = 5  -- Always use 5 as the teleport distance
-			chr:TranslateBy(hum.MoveDirection * tpValue * delta * 10)  -- Apply teleportation
-		end
-	end
+    if hum and hum.MoveDirection.Magnitude > 0 then
+        if tpwalking then
+            local tpValue = 5  -- Always use 5 as the teleport distance
+            chr:TranslateBy(hum.MoveDirection * tpValue * delta * 10)  -- Apply teleportation
+        end
+    end
+
+    -- Toggle tpwalking on and off each time the script runs
+    tpwalking = not tpwalking
 end)

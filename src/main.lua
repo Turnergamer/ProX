fc207775257c1ffbc190879d6b7ee51c88459ec2
Turnergamer <63259043+Turnergamer@users.noreply.github.com>
2025-01-31 05:08:08 +0000

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -626,7 +626,7 @@ local function invokeRollRemote()
 	local remoteFunctionArgs = {
 		[1] = "PremRollSkin10"
 	}
-	wait(5)
+	wait(30)
 	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(remoteFunctionArgs))
 end
 

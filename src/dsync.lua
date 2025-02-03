3096a335e704bd1ea1af7bae55b23a052d04d3b7
Turnergamer <63259043+Turnergamer@users.noreply.github.com>
2025-02-03 05:49:01 +0000

    Update dsync.lua

--- src/dsync.lua
+++ src/dsync.lua
@@ -4,37 +4,38 @@ getgenv().underground = -3.6
 local enabled = false
 local runserv = game:GetService("RunService")
 local lplr = game:GetService("Players").LocalPlayer
-local mouse = lplr:GetMouse()
 local animation = Instance.new("Animation")
 animation.AnimationId = "http://www.roblox.com/asset/?id=15693621070"
 local danceTrack
-
--- Check if _G.DsyncCheck is true, and enable or disable the feature accordingly
-if _G.DsyncCheck then
-    -- Enable dsync (like the previous behavior)
-    pcall(function()
-        if enabled == false then
-            enabled = true
-            danceTrack = lplr.Character:FindFirstChildWhichIsA("Humanoid"):LoadAnimation(animation)
-            danceTrack.Looped = false
-            danceTrack:Play(.1, 1, 0)
-            print("Dsync is enabled!")
-        end
-    end)
-else
-    -- Disable dsync if _G.DsyncCheck is false
-    if enabled then
-        enabled = false
-        if danceTrack then
-            danceTrack:Stop()
-            danceTrack:Destroy()
-        end
-        print("Dsync is disabled!")
-    end
-end
-
 local dysenc = {}
 local temp = 1
+
+_G.DsyncCheck = _G.DsyncCheck or false  -- Default to false if not set
+
+spawn(function()
+    while true do
+        if _G.DsyncCheck then
+            pcall(function()
+                if not enabled then
+                    enabled = true
+                    danceTrack = lplr.Character:FindFirstChildWhichIsA("Humanoid"):LoadAnimation(animation)
+                    danceTrack.Looped = false
+                    danceTrack:Play(.1, 1, 0)
+                end
+            end)
+        else
+            if enabled then
+                enabled = false
+                if danceTrack then
+                    danceTrack:Stop()
+                    danceTrack:Destroy()
+                end
+            end
+        end
+        wait(0.5) -- Adjust timing to optimize performance
+    end
+end)
+
 runserv.Heartbeat:Connect(function()
     temp = temp + 1
     if enabled and lplr.Character and lplr.Character.HumanoidRootPart then
